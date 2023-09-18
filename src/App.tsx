e0c2c3adb354788102e8e12bdac42183ae99389e
Andrey <95442676+AndreParkh@users.noreply.github.com>
2023-09-18 06:09:16 +0000

    Update App.tsx and Calendar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { useState } from 'react';
-import './scss/styles.css';
+import { useState } from "react";
+import "./scss/styles.css";
 
-import { Calendar } from './components/Calendar';
-import {CalendarIcon} from "./components/Icon"
+import { Calendar } from "./components/Calendar";
+import { CalendarIcon } from "./components/Icon";
 
 function App() {
-	const [selectedDate, setSelectedDate] = useState(new Date)
-	const [isShownCalendar, setIsShownCalendar] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isShownCalendar, setIsShownCalendar] = useState(false);
 
   return (
     <div className="App">
-
-		<div className="calendar">
-			{isShownCalendar && <Calendar 
-				selectedDate={selectedDate} 
-				selectDate={(date) => setSelectedDate(date)} />}
-			<button 
-				onClick={() => {
-					setIsShownCalendar(!isShownCalendar)
-					if (isShownCalendar) setSelectedDate(new Date())
-				}}
-				className={[
-					"btnShowCalendar",
-					isShownCalendar ? "btnShowCalendar__active": ''
-					].join(' ')}>
-				<CalendarIcon />
-			</button>
-		</div>
+      <div className="calendar">
+        {isShownCalendar && (
+          <Calendar
+            selectedDate={selectedDate}
+            selectDate={(date) => setSelectedDate(date)}
+          />
+        )}
+        <button
+          onClick={() => {
+            setIsShownCalendar(!isShownCalendar);
+            if (isShownCalendar) setSelectedDate(new Date());
+          }}
+          className={[
+            "btnShowCalendar",
+            isShownCalendar ? "btnShowCalendar__active" : "",
+          ].join(" ")}
+        >
+          <CalendarIcon />
+        </button>
+      </div>
     </div>
   );
 }
